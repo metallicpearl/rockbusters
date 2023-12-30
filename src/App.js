f0d8463b981493993e65ab6be5c6d748ca9377b6
Phill Vernon <borismcbobble@hotmail.com>
2023-12-30 17:01:20 +0000

    iOs

--- src/App.js
+++ src/App.js
@@ -586,9 +586,9 @@ function App() {
           <span>
             <input id='answerText' hidden={answerInputHidden}
               type="text" ref={answerInput} onChange={(event) => { updateAnswerState(event.target.value); }} onKeyDown={(event) => { if (event.key == 'Enter') { checkAnswer() }; }}
-              style={{ alignContent: 'center', width: "70%", backgroundColor: 'beige', borderRadius:5, border: "2px solid", fontWeight: 'bolder', fontSmooth: 'always', fontFamily: 'Calibri', fontSize: 35}}>
+              style={{ alignContent: 'center', width: "60%", backgroundColor: 'beige', borderRadius:5, border: "2px solid", fontWeight: 'bolder', fontSmooth: 'always', fontFamily: 'Calibri', fontSize: 35}}>
             </input>
-            <label onClick={async () => { checkAnswer();}} hidden={buttonHideState} style={{ paddingTop: 1, whiteSpace: 'pre-wrap', margin: 5, borderRadius: 5, backgroundColor: finishIsHover ? 'lightgreen' : 'green', width: 100, border: '5px dotted', borderColor: finishIsHover ? 'lightgreen' : 'green', fontFamily: 'Calibri', fontSmooth: 'always', fontSize: 34, fontWeight: 'bolder', color: finishIsHover ? 'black' : 'white', alignContent: 'center' }}>
+            <label onClick={async () => { checkAnswer();}} hidden={buttonHideState} style={{ paddingTop: 1, whiteSpace: 'pre-wrap', margin: 5, borderRadius: 5, backgroundColor: finishIsHover ? 'lightgreen' : 'green', width: "30%", border: '5px dotted', borderColor: finishIsHover ? 'lightgreen' : 'green', fontFamily: 'Calibri', fontSmooth: 'always', fontSize: 34, fontWeight: 'bolder', color: finishIsHover ? 'black' : 'white', alignContent: 'center' }}>
               {" SUBMIT "}
             </label>
             <label
@@ -709,7 +709,7 @@ function App() {
           <span maxWidth="90%">
             <input id='answerText' hidden={answerInputHidden}
               type="text" ref={answerInput} onChange={(event) => { updateAnswerState(event.target.value); }} onKeyDown={(event) => { if (event.key == 'Enter') { checkAnswer() }; }}
-              style={{ alignContent: 'center', width: "70%", backgroundColor: 'beige', borderRadius: 5, border: "2px solid", fontWeight: 'bolder', fontSmooth: 'always', fontFamily: 'Calibri', fontSize: 22, borderRadius: 5}}>
+              style={{ alignContent: 'center', width: "60%", backgroundColor: 'beige', borderRadius: 5, border: "2px solid", fontWeight: 'bolder', fontSmooth: 'always', fontFamily: 'Calibri', fontSize: 22, borderRadius: 5}}>
             </input>
             <label onClick={async () => { checkAnswer();}} hidden={buttonHideState} style={{ margin: 10, marginInline:5, whiteSpace: 'pre-wrap', borderRadius:5, backgroundColor: finishIsHover ? 'lightgreen' : 'green', border: '4px dotted', borderColor: finishIsHover ? 'lightgreen' : 'green', fontFamily: 'Calibri', fontSmooth: 'always', fontSize: 18, fontWeight: 'bolder', color: finishIsHover ? 'black' : 'white', alignContent: 'center', paddingTop: 3 }}>
               {" SUBMIT "}
